5431c1ef5eb59c0d66d3f6d9c00434061ca244f8
Shreyash Shendkar <shreyash.shendkar@pw.live>
2025-05-29 14:44:41 +0530

    test 3

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -19,9 +19,10 @@ export default function Page2() {
   },[]);
 const convertdate=(date)=>{
   let ts = new Date(date);
-  let new_timezone = 'Asia/Kolkata';
+  let new_timezone = '';
   let options = { timeZone: new_timezone, hour12: false, weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
   let formatted_date = ts.toLocaleString('en-US', options);
+  return formatted_date;
 }
   return (
     <>
