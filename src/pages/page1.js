0e9fc0c507f81043385e821fb34f550459d64e46
Shreyash Shendkar <shreyash.shendkar@pw.live>
2025-05-29 13:09:33 +0530

    test 42

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -24,7 +24,7 @@ export default function Page1() {
       fields: '*',
     },
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
       'X-RapidAPI-Key': '768be3d199msh0cf59e6e372e153p131e34jsn3a97a3b820b4',
       'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
     },
