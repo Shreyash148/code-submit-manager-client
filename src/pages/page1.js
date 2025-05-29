f12f38dbf6c28a3addb2504a806834a6fe578613
Shreyash Shendkar <shreyash.shendkar@pw.live>
2025-05-29 07:52:26 +0530

    test 17

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -57,6 +57,7 @@ export default function Page1() {
         sourcecode:submission.sourcecode,
         stdin:submission.stdin,
         username:submission.username,
+        stdout:endcodedString
       })
       alert("Submission Done");
       navigate("/page2");
